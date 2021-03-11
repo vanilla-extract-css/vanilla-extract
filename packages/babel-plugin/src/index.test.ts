3605eedc1c525eadfa6a01ee996d1c2373d43bee
Matt Jones <mattjones701@gmail.com>
2021-03-11 13:28:52 +1100

    Add automatic debug ids from babel plugin

--- packages/babel-plugin/src/index.test.ts
+++ packages/babel-plugin/src/index.test.ts
@@ -21,59 +21,266 @@ const transform = (
 };
 
 describe('babel plugin', () => {
-  it("should not add the header when lib isn't used", () => {
-    const source = `
-      import { foo } from 'bar';
-
-      const one = foo({
-        zIndex: 2,
-      });
-    `;
-
-    expect(transform(source)).toMatchInlineSnapshot(`
-      "import { foo } from 'bar';
-      const one = foo({
-        zIndex: 2
-      });"
-    `);
-  });
-
-  it('should add the header when using lib', () => {
+  it('should handle style assigned to const', () => {
     const source = `
       import { style } from '@mattsjones/css-core';
 
       const one = style({
-        zIndex: 2,
+          zIndex: 2,
       });
     `;
 
     expect(transform(source)).toMatchInlineSnapshot(`
-      "import { setFileScope } from \\"@mattsjones/css-core\\";
-      setFileScope('/my/app/dir/mockFilename.treat.ts');
-      import { style } from '@mattsjones/css-core';
+      "import { style } from '@mattsjones/css-core';
       const one = style({
         zIndex: 2
-      });"
+      }, \\"one\\");"
     `);
   });
 
-  it('should add the header when using alised lib', () => {
+  it('should handle style assigned to default export', () => {
     const source = `
-      import { style } from 'my-alias/treat';
+      import { style } from '@mattsjones/css-core';
 
-      const one = style({
-        zIndex: 2,
+      export default style({
+          zIndex: 2,
       });
     `;
 
-    expect(transform(source, { alias: 'my-alias/treat' }))
+    expect(transform(source)).toMatchInlineSnapshot(`
+      "import { style } from '@mattsjones/css-core';
+      export default style({
+        zIndex: 2
+      }, \\"mockFilename\\");"
+    `);
+  });
+
+  it('should handle style assigned to default export in index.js file', () => {
+    const source = `
+      import { style } from '@mattsjones/css-core';
+
+      export default style({
+          zIndex: 2,
+      });
+    `;
+
+    expect(transform(source, {}, '/someFolder/index.js'))
       .toMatchInlineSnapshot(`
-      "import { setFileScope } from \\"my-alias/treat\\";
-      setFileScope('/my/app/dir/mockFilename.treat.ts');
-      import { style } from 'my-alias/treat';
-      const one = style({
+        "import { style } from '@mattsjones/css-core';
+        export default style({
+          zIndex: 2
+        }, \\"someFolder\\");"
+      `);
+  });
+
+  it('should handle style assigned to object property', () => {
+    const source = `
+      import { style } from '@mattsjones/css-core';
+
+      const test = {
+        one: {
+          two: style({
+            zIndex: 2,
+          })
+        }
+      };
+    `;
+
+    expect(transform(source)).toMatchInlineSnapshot(`
+      "import { style } from '@mattsjones/css-core';
+      const test = {
+        one: {
+          two: style({
+            zIndex: 2
+          }, \\"test_one_two\\")
+        }
+      };"
+    `);
+  });
+
+  it('should handle style returned from an arrow function', () => {
+    const source = `
+      import { style } from '@mattsjones/css-core';
+
+      const test = () => {
+        return style({
+          color: 'red'
+        });
+      };
+    `;
+
+    expect(transform(source)).toMatchInlineSnapshot(`
+      "import { style } from '@mattsjones/css-core';
+      
+      const test = () => {
+        return style({
+          color: 'red'
+        }, \\"test\\");
+      };"
+    `);
+  });
+
+  it('should handle style returned from a function', () => {
+    const source = `
+      import { style } from '@mattsjones/css-core';
+
+      function test() {
+        return style({
+          color: 'red'
+        });
+      }
+    `;
+
+    expect(transform(source)).toMatchInlineSnapshot(`
+      "import { style } from '@mattsjones/css-core';
+      
+      function test() {
+        return style({
+          color: 'red'
+        }, \\"test\\");
+      }"
+    `);
+  });
+
+  it('should handle createTheme assigned to const', () => {
+    const source = `
+      import { createTheme } from '@mattsjones/css-core';
+
+      const darkTheme = createTheme({}, {});
+    `;
+
+    expect(transform(source)).toMatchInlineSnapshot(`
+      "import { createTheme } from '@mattsjones/css-core';
+      const darkTheme = createTheme({}, {}, \\"darkTheme\\");"
+    `);
+  });
+
+  it('should ignore functions that already supply a debug name', () => {
+    const source = `
+      import { style } from '@mattsjones/css-core';
+
+      const three = style({
+          testStyle: {
+            zIndex: 2,
+          }
+      }, 'myDebugValue');
+    `;
+
+    expect(transform(source)).toMatchInlineSnapshot(`
+      "import { style } from '@mattsjones/css-core';
+      const three = style({
+        testStyle: {
+          zIndex: 2
+        }
+      }, 'myDebugValue');"
+    `);
+  });
+
+  it('should only apply to functions imported from the relevant package', () => {
+    const source = `
+      import { style } from 'treats';
+
+      const three = style({
+        zIndex: 2,  
+      });
+    `;
+
+    expect(transform(source)).toMatchInlineSnapshot(`
+      "import { style } from 'treats';
+      const three = style({
         zIndex: 2
       });"
+    `);
+  });
+
+  it('should handle renaming imports', () => {
+    const source = `
+      import { style as specialStyle } from '@mattsjones/css-core';
+
+      const four = specialStyle({
+        zIndex: 2,  
+      });
+    `;
+
+    expect(transform(source)).toMatchInlineSnapshot(`
+      "import { style as specialStyle } from '@mattsjones/css-core';
+      const four = specialStyle({
+        zIndex: 2
+      }, \\"four\\");"
+    `);
+  });
+
+  it('should handle package aliases', () => {
+    const source = `
+      import { style } from 'my-alias';
+
+      const four = style({
+        zIndex: 2,  
+      });
+    `;
+
+    expect(transform(source, { alias: 'my-alias' })).toMatchInlineSnapshot(`
+        "import { style } from 'my-alias';
+        const four = style({
+          zIndex: 2
+        }, \\"four\\");"
+      `);
+  });
+
+  it('should handle anonymous style in arrays', () => {
+    const source = `
+       import { style } from '@mattsjones/css-core';
+
+       export const height = [
+        style({
+          zIndex: 2,  
+        })
+      ];
+    `;
+
+    expect(transform(source)).toMatchInlineSnapshot(`
+      "import { style } from '@mattsjones/css-core';
+      export const height = [style({
+        zIndex: 2
+      }, \\"height\\")];"
+    `);
+  });
+
+  it('should handle object key with anonymous style in arrays', () => {
+    const source = `
+       import { style } from '@mattsjones/css-core';
+
+       export const height = {
+        full: [style({
+          zIndex: 2,  
+        })]
+       };
+    `;
+
+    expect(transform(source)).toMatchInlineSnapshot(`
+      "import { style } from '@mattsjones/css-core';
+      export const height = {
+        full: [style({
+          zIndex: 2
+        }, \\"height_full\\")]
+      };"
+    `);
+  });
+
+  it('should handle namespace imports', () => {
+    const source = `
+      import * as css from '@mattsjones/css-core';
+
+      const one = css.style({
+          zIndex: 2,
+      });
+    `;
+
+    expect(transform(source)).toMatchInlineSnapshot(`
+      "import * as css from '@mattsjones/css-core';
+      const one = css.style({
+        zIndex: 2
+      }, \\"one\\");"
     `);
   });
 });
--- packages/babel-plugin/src/index.ts
+++ packages/babel-plugin/src/index.ts
@@ -1,46 +1,148 @@
-import { types as t, NodePath, template } from '@babel/core';
-import { Options } from './types';
+import { dirname, basename } from 'path';
+import { types as t, PluginObj, PluginPass, NodePath } from '@babel/core';
 
-type Context = { filename: string; hasTreatImports: boolean };
+const exportConfig = {
+  style: {
+    maxParams: 2,
+  },
+  createTheme: {
+    maxParams: 3,
+  },
+};
+type RelevantExport = keyof typeof exportConfig;
+const relevantExports = Object.keys(exportConfig) as Array<RelevantExport>;
 
-export default function () {
+const extractName = (node: t.Node, fileIdentifier: string) => {
+  if (t.isObjectProperty(node) && t.isIdentifier(node.key)) {
+    return node.key.name;
+  } else if (
+    (t.isVariableDeclarator(node) || t.isFunctionDeclaration(node)) &&
+    t.isIdentifier(node.id)
+  ) {
+    return node.id.name;
+  } else if (t.isExportDefaultDeclaration(node)) {
+    return fileIdentifier;
+  }
+};
+
+const getDebugId = (
+  path: NodePath<t.CallExpression>,
+  fileIdentifier: string,
+) => {
+  const { parent } = path;
+
+  if (
+    t.isObjectProperty(parent) ||
+    t.isReturnStatement(parent) ||
+    t.isArrayExpression(parent) ||
+    t.isSpreadElement(parent)
+  ) {
+    const names: Array<string> = [];
+
+    path.findParent(({ node: parentNode }) => {
+      const name = extractName(parentNode, fileIdentifier);
+      if (name) {
+        names.unshift(name);
+      }
+      // Traverse all the way to the root
+      return false;
+    });
+
+    return names.join('_');
+  } else {
+    return extractName(parent, fileIdentifier);
+  }
+};
+
+const getRelevantCall = (
+  node: t.CallExpression,
+  namespaceImport: string,
+  importIdentifiers: Map<string, RelevantExport>,
+) => {
+  const { callee } = node;
+
+  if (
+    namespaceImport &&
+    t.isMemberExpression(callee) &&
+    t.isIdentifier(callee.object, { name: namespaceImport })
+  ) {
+    return relevantExports.find((exportName) =>
+      t.isIdentifier(callee.property, { name: exportName }),
+    );
+  } else {
+    const importInfo = Array.from(
+      importIdentifiers.entries(),
+    ).find(([identifier]) => t.isIdentifier(callee, { name: identifier }));
+
+    if (importInfo) {
+      return importInfo[1];
+    }
+  }
+};
+
+interface PluginOptions {
+  alias?: string;
+}
+type Context = PluginPass & {
+  opts?: PluginOptions;
+  fileIdentifier: string;
+  namespaceImport: string;
+  importIdentifiers: Map<string, RelevantExport>;
+};
+
+export default function (): PluginObj<Context> {
   return {
-    pre(this: Context, state: any) {
-      this.filename = state.opts.filename;
-      this.hasTreatImports = false;
+    pre(state) {
+      const filename = state.opts.filename || '';
+      const shortFilename = basename(filename).split('.')[0];
+
+      this.fileIdentifier =
+        shortFilename.indexOf('index') > -1
+          ? basename(dirname(filename))
+          : shortFilename;
+
+      this.importIdentifiers = new Map();
+      this.namespaceImport = '';
+      this.packageIdentifier = this.opts?.alias || '@mattsjones/css-core';
     },
     visitor: {
-      ImportDeclaration(
-        this: Context,
-        path: NodePath<t.ImportDeclaration>,
-        { opts }: { opts: Options },
-      ) {
-        const treatImportIdentifier = opts.alias || '@mattsjones/css-core';
+      ImportDeclaration(path) {
+        if (path.node.source.value === this.packageIdentifier) {
+          path.node.specifiers.forEach((specifier) => {
+            if (t.isImportNamespaceSpecifier(specifier)) {
+              this.namespaceImport = specifier.local.name;
+            } else if (t.isImportSpecifier(specifier)) {
+              const { imported, local } = specifier;
 
-        if (path.node.source.value === treatImportIdentifier) {
-          this.hasTreatImports = true;
+              const importName = (t.isIdentifier(imported)
+                ? imported.name
+                : imported.value) as RelevantExport;
+
+              if (relevantExports.includes(importName)) {
+                this.importIdentifiers.set(local.name, importName);
+              }
+            }
+          });
         }
       },
-      Program: {
-        exit(
-          this: Context,
-          path: NodePath<t.Program>,
-          { opts }: { opts: Options },
-        ) {
-          if (this.hasTreatImports) {
-            const treatPackageName = opts.alias || '@mattsjones/css-core';
-            // TODO ensure filename is escaped
-            const setFileScope = template(
-              `
-                import { setFileScope } from "${treatPackageName}";
-                setFileScope('${this.filename}');
-              `,
-              { sourceType: 'module' },
-            );
+      CallExpression(path) {
+        const { node } = path;
 
-            path.get('body')[0].insertBefore(setFileScope());
+        const usedExport = getRelevantCall(
+          node,
+          this.namespaceImport,
+          this.importIdentifiers,
+        );
+
+        if (usedExport) {
+          if (node.arguments.length < exportConfig[usedExport].maxParams) {
+            const debugIdent = getDebugId(path, this.fileIdentifier);
+
+            if (debugIdent) {
+              node.arguments.push(t.stringLiteral(debugIdent));
+            }
           }
-        },
+        }
       },
     },
   };
