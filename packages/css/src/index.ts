3ee1fa0cff1755ff134c29c35a80709021c8c483
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2021-04-12 16:16:15 +1000

    Remove duplicate code

--- packages/css/src/index.ts
+++ packages/css/src/index.ts
@@ -1,7 +1,6 @@
 import './runtimeAdapter';
 
 export type { StyleRule, GlobalStyleRule, Adapter, FileScope } from './types';
-export { assignVars } from './assignVars';
 export * from './identifier';
 export * from './theme';
 export * from './style';
