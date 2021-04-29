1ea5c45fa795fd02e0bf1a4e81d5a93d6eb48945
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2021-04-29 10:27:11 +1000

    WIP

--- examples/webpack/src/shared.css.ts
+++
@@ -1,9 +0,0 @@
-import { globalStyle, style } from '@vanilla-extract/css';
-
-export const shadow = style({
-  boxShadow: '0 0 5px red',
-});
-
-globalStyle('body', {
-  backgroundColor: 'skyblue',
-});
