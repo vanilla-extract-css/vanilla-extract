bc857cfbb5a127fb664d78721d448cf19dbe7154
Matt Jones <mattjones701@gmail.com>
2021-04-30 23:12:06 +1000

    Revert

--- packages/dynamic/src/index.ts
+++ packages/dynamic/src/index.ts
@@ -1,5 +1,3 @@
-export { getVarName } from '@vanilla-extract/private';
-
 export { createInlineTheme } from './createInlineTheme';
 export { setElementTheme } from './setElementTheme';
 export { setElementVar } from './setElementVar';
