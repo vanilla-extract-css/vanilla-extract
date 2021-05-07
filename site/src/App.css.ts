e52ac5c8787a035548c93d21ef36e03245cedee8
Michael Taranto <mtaranto@seek.com.au>
2021-05-07 15:50:58 +1000

    Unstyle scrollbars

--- site/src/App.css.ts
+++ site/src/App.css.ts
@@ -1,11 +1,10 @@
-import { globalStyle, createVar } from '@vanilla-extract/css';
+import { globalStyle } from '@vanilla-extract/css';
 import { darkMode } from './system/styles/atoms.css';
 import { vars } from './themes.css';
 
 globalStyle('html, body', {
   margin: 0,
   padding: 0,
-  overflow: 'overlay',
 });
 
 globalStyle(`.${darkMode}`, {
@@ -15,30 +14,4 @@ globalStyle(`.${darkMode}`, {
 globalStyle('html', {
   background: vars.palette.white,
   color: vars.palette.coolGray900,
-});
-
-const scrollBarColor = createVar();
-const scrollBarTrackColor = createVar();
-
-globalStyle(`*`, {
-  vars: {
-    [scrollBarColor]: vars.palette.coolGray400,
-    [scrollBarTrackColor]: vars.palette.coolGray50,
-  },
-  scrollbarColor: `${scrollBarColor} ${scrollBarTrackColor}`,
-  scrollbarWidth: 'thin',
-});
-globalStyle(`.${darkMode}, .${darkMode} *`, {
-  vars: {
-    [scrollBarColor]: vars.palette.gray600,
-    [scrollBarTrackColor]: vars.palette.gray900,
-  },
-});
-globalStyle(`*::-webkit-scrollbar`, {
-  width: vars.spacing.small,
-  height: vars.spacing.small,
-});
-globalStyle(`*::-webkit-scrollbar-thumb`, {
-  backgroundColor: scrollBarColor,
-  borderRadius: vars.border.radius.small,
 });
