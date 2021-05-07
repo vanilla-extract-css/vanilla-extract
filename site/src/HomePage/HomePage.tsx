c95888a1094950e8f9fb1c67c78d7f3d19e118d9
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2021-05-07 10:02:32 +1000

    Add tweet

--- site/src/HomePage/HomePage.tsx
+++ site/src/HomePage/HomePage.tsx
@@ -370,8 +370,8 @@ export const HomePage = () => {
                   Organise your styles with ease
                 </Heading>
                 <Text align="center" color="secondary">
-                  Group multiple style variants into a single collection, then
-                  look them up by name. No awkward naming conventions required.
+                  Group style variants into separate collections, then look them
+                  up by name. No awkward naming conventions required.
                 </Text>
               </Stack>
 
@@ -440,8 +440,30 @@ export const HomePage = () => {
             background={{ lightMode: 'blue100', darkMode: 'gray900' }}
             className={styles.skewedContainerSecondary}
           >
-            <Stack space="xxlarge" align="center">
-              <ContentBlock size="large" withGutters>
+            <ContentBlock size="large" withGutters>
+              <Stack space="xxlarge" align="center">
+                <Columns space="xxlarge" collapseOnTablet alignY="center">
+                  <Tweet
+                    handle="@jeresig"
+                    name="John Resig"
+                    avatar="https://pbs.twimg.com/profile_images/1090714620275245056/HS9xcEDk_200x200.jpg"
+                    url="https://twitter.com/jeresig/status/1375609805373575175"
+                  >
+                    vanilla-extract is super exciting - it’s scratching an itch
+                    that we have at @khanacademy, as we look to move off of
+                    Aphrodite to something that has better perf characteristics.
+                    We were thinking CSS Modules but this is even more ideal!
+                  </Tweet>
+                  <Tweet
+                    handle="@okonetchnikov"
+                    name="Andrey Okonetchnikov"
+                    avatar="https://pbs.twimg.com/profile_images/1327698924698804226/io9KdHy__400x400.jpg"
+                    url="https://twitter.com/okonetchnikov/status/1389450586257514505"
+                  >
+                    I really don’t want to rework my whole workshop but this is
+                    looking so good I almost have to 😅🤔
+                  </Tweet>
+                </Columns>
                 <Columns space="xxlarge" collapseOnTablet alignY="center">
                   <Tweet
                     handle="@lorvsso"
@@ -454,51 +476,22 @@ export const HomePage = () => {
                     I ended up recommending and implementing. This is CSS
                     Modules for the new decade ❤️😍
                   </Tweet>
-
                   <Tweet
-                    handle="@jeresig"
-                    name="John Resig"
-                    avatar="https://pbs.twimg.com/profile_images/1090714620275245056/HS9xcEDk_200x200.jpg"
-                    url="https://twitter.com/jeresig/status/1375609805373575175"
+                    handle="@kossnocorp"
+                    name="Sasha Koss"
+                    avatar="https://pbs.twimg.com/profile_images/979030533719064576/rD33B86M_400x400.jpg"
+                    url="https://twitter.com/kossnocorp/status/1390214753352658946"
                   >
-                    vanilla-extract is super exciting - it’s scratching an itch
-                    that we have at @khanacademy, as we look to move off of
-                    Aphrodite to something that has better perf characteristics.
-                    We were thinking CSS Modules but this is even more ideal!
+                    vanilla-extract is the first CSS-in-JS library that excites
+                    me and convinces me to ditch CSS modules in favor of it.
+                    TypeScript support is superb 👌
+                    <br />
+                    <br />
+                    Looking forward to rewriting my design system using it!
                   </Tweet>
-                  {/* 
-                <Tweet
-                  handle="@jevakallio"
-                  name="Macbook Miller"
-                  avatar="https://pbs.twimg.com/profile_images/1260968240504856576/FNIWLC0A_200x200.jpg"
-                  url="https://twitter.com/jevakallio/status/1375362320122183684"
-                >
-                  You had me at vanilla-extract
-                </Tweet> */}
-                  <Tweet
-                    handle="@okonetchnikov"
-                    name="Andrey Okonetchnikov"
-                    avatar="https://pbs.twimg.com/profile_images/1327698924698804226/io9KdHy__400x400.jpg"
-                    url="https://twitter.com/okonetchnikov/status/1389450586257514505"
-                  >
-                    I really don’t want to rework my whole workshop but this is
-                    looking so good I almost have to 😅🤔
-                  </Tweet>
-
-                  {/* <Tweet
-                  handle="@markdalgleish"
-                  name="Mark Dalgleish"
-                  avatar="https://pbs.twimg.com/profile_images/754886061872979968/BzaOWhs1_200x200.jpg"
-                  url="https://twitter.com/markdalgleish/status/1375371778306887681"
-                >
-                  Huge shout out to @peduarte for his amazing work on
-                  @stitchesjs, leading the way and showing everyone how
-                  first-class CSS Variables can be a core feature of modern
-                  CSS-in-JS libraries.
-                </Tweet> */}
                 </Columns>
-              </ContentBlock>
-            </Stack>
+              </Stack>
+            </ContentBlock>
           </Box>
         </Box>
 
