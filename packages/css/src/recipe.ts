8d63e6f5c9e5924137478d3b6813ef738499fb4a
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2021-05-12 14:47:29 +1000

    Improve addRecipe types

--- packages/css/src/recipe.ts
+++ packages/css/src/recipe.ts
@@ -1,10 +1,19 @@
+type Primitive = string | number | null | undefined;
+
+type Serializable = {
+  [Key in string | number]: Primitive | Serializable;
+};
+
 interface Recipe {
   importPath: string;
   importName: string;
-  args: ReadonlyArray<unknown>;
+  args: ReadonlyArray<Serializable>;
 }
 
-export function addRecipe<Target>(target: Target, recipe: Recipe) {
+export function addRecipe<Target extends object>(
+  target: Target,
+  recipe: Recipe,
+) {
   // @ts-expect-error This references the global adapter bound by the integration package to detect whether we're in a .css.ts context
   if (typeof __adapter__ !== 'undefined') {
     Object.defineProperty(target, '__recipe__', {
