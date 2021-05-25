613db1a420e1a12d997d97912acc4b82688ce42b
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2021-05-25 14:34:17 +1000

    Sprinkles: Add RequiredConditionalValue type

--- packages/sprinkles/src/types.ts
+++ packages/sprinkles/src/types.ts
@@ -1,21 +1,45 @@
-interface Tuple<Length extends number, Value> extends ReadonlyArray<Value> {
+interface ResponsiveArray<Length extends number, Value>
+  extends ReadonlyArray<Value> {
   0: Value;
   length: Length;
 }
 
-export type ResponsiveArrayConfig<Value> = Tuple<2 | 3 | 5 | 6 | 7 | 8, Value>;
+interface RequiredResponsiveArray<Length extends number, Value>
+  extends ReadonlyArray<Value> {
+  0: Exclude<Value, null>;
+  length: Length;
+}
 
-export type ResponsiveArray<Count extends number, Value> = [
+export type ResponsiveArrayConfig<
+  Value extends string | number
+> = ResponsiveArray<2 | 3 | 5 | 6 | 7 | 8, Value>;
+
+export type ResponsiveArrayByMaxLength<MaxLength extends number, Value> = [
   never,
-  Tuple<1, Value>,
-  Tuple<1 | 2, Value>,
-  Tuple<1 | 2 | 3, Value>,
-  Tuple<1 | 2 | 3 | 4, Value>,
-  Tuple<1 | 2 | 3 | 4 | 5, Value>,
-  Tuple<1 | 2 | 3 | 4 | 5 | 6, Value>,
-  Tuple<1 | 2 | 3 | 4 | 5 | 6 | 7, Value>,
-  Tuple<1 | 2 | 3 | 4 | 5 | 6 | 7 | 8, Value>,
-][Count];
+  ResponsiveArray<1, Value | null>,
+  ResponsiveArray<1 | 2, Value | null>,
+  ResponsiveArray<1 | 2 | 3, Value | null>,
+  ResponsiveArray<1 | 2 | 3 | 4, Value | null>,
+  ResponsiveArray<1 | 2 | 3 | 4 | 5, Value | null>,
+  ResponsiveArray<1 | 2 | 3 | 4 | 5 | 6, Value | null>,
+  ResponsiveArray<1 | 2 | 3 | 4 | 5 | 6 | 7, Value | null>,
+  ResponsiveArray<1 | 2 | 3 | 4 | 5 | 6 | 7 | 8, Value | null>,
+][MaxLength];
+
+export type RequiredResponsiveArrayByMaxLength<
+  MaxLength extends number,
+  Value
+> = [
+  never,
+  RequiredResponsiveArray<1, Value | null>,
+  RequiredResponsiveArray<1 | 2, Value | null>,
+  RequiredResponsiveArray<1 | 2 | 3, Value | null>,
+  RequiredResponsiveArray<1 | 2 | 3 | 4, Value | null>,
+  RequiredResponsiveArray<1 | 2 | 3 | 4 | 5, Value | null>,
+  RequiredResponsiveArray<1 | 2 | 3 | 4 | 5 | 6, Value | null>,
+  RequiredResponsiveArray<1 | 2 | 3 | 4 | 5 | 6 | 7, Value | null>,
+  RequiredResponsiveArray<1 | 2 | 3 | 4 | 5 | 6 | 7 | 8, Value | null>,
+][MaxLength];
 
 export type ConditionalPropertyValue = {
   defaultClass: string | undefined;
