922f24a3a0062ea66a44de5824081eb6e2c3d370
Matt Jones <mattjones701@gmail.com>
2021-07-16 11:44:31 +1000

    Fix `createGlobalTheme` types when accepting an existing contract

---
+++ .changeset/eleven-colts-admire.md
@@ -0,0 +1,6 @@
+---
+'@vanilla-extract/css': patch
+'@vanilla-extract/private': patch
+---
+
+Fix `createGlobalTheme` types when accepting an existing contract
--- packages/private/src/index.ts
+++ packages/private/src/index.ts
@@ -1,4 +1,4 @@
-export type { Contract, MapLeafNodes } from './types';
+export type { Contract, MapLeafNodes, CSSVarFunction } from './types';
 export { getVarName } from './getVarName';
 export { get } from './get';
 export { walkObject } from './walkObject';
