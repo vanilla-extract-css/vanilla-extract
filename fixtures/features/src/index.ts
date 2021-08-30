de7e1d2532cb8afb5e150cbaa5e2a6b4036f1cd9
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2021-08-30 13:18:34 +1000

    Add more test cases

--- fixtures/features/src/index.ts
+++ fixtures/features/src/index.ts
@@ -1,12 +1,15 @@
-import * as styles from './composedStyles.css';
+import * as styles from './features.css';
 import testNodes from '../test-nodes.json';
 
 function render() {
   document.body.innerHTML = `
     <div id="${testNodes.mergedStyle}" class="${styles.mergedStyle}">Merged style</div>
     <div id="${testNodes.styleWithComposition}" class="${styles.styleWithComposition}">Style with composition</div>
+    <div id="${testNodes.styleVariantsWithComposition}" class="${styles.styleVariantsWithComposition.variant}">Style variants with composition</div>
+    <div id="${testNodes.styleVariantsWithMappedComposition}" class="${styles.styleVariantsWithMappedComposition.variant}">Style variants with mapped composition</div>
     <div id="${testNodes.compositionOnly}" class="${styles.compositionOnly}">Composition only</div>
-    <div id="${testNodes.compositionInSelector}" class="${styles.compositionInSelector}">Composition in selector</div>
+    <div id="${testNodes.styleCompositionInSelector}" class="${styles.styleCompositionInSelector}">Style composition in selector</div>
+    <div id="${testNodes.styleVariantsCompositionInSelector}" class="${styles.styleVariantsCompositionInSelector.variant}">Style variants composition in selector</div>
   `;
 }
 
