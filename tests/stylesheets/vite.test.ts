d425728a32339de5f5079bb33ead08e3e60bc858
mattcompiles <mattjones701@gmail.com>
2021-09-06 11:08:06 +1000

    Move E2E tests to screenshots based tests (#337)

---
+++ tests/stylesheets/vite.test.ts
@@ -0,0 +1,30 @@
+import { getStylesheet, startFixture, TestServer } from 'test-helpers';
+
+const workerIndex = parseInt(process.env.JEST_WORKER_ID ?? '', 10);
+
+const fixtures = ['unused-modules', 'themed', 'sprinkles', 'features'];
+
+let testCounter = 0;
+
+describe.each(fixtures)('vite - %s', (fixture) => {
+  let server: TestServer;
+
+  beforeAll(async () => {
+    const portRange = 100 * workerIndex;
+    server = await startFixture(fixture, {
+      type: 'vite',
+      mode: 'production',
+      basePort: 12000 + portRange + testCounter++,
+    });
+  });
+
+  it('should create valid stylesheet', async () => {
+    expect(
+      await getStylesheet(server.url, server.stylesheet),
+    ).toMatchSnapshot();
+  });
+
+  afterAll(async () => {
+    await server.close();
+  });
+});
