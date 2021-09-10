c2f92bd4540d4ada04f54e1af6aa58c22a342022
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2021-09-10 12:23:28 +1000

    Add Recipes API docs to nav

--- site/contents.js
+++ site/contents.js
@@ -1,7 +1,8 @@
 module.exports = [
   { fileName: 'getting-started.md', id: 'getting-started', root: true },
   { fileName: 'styling-api.md', id: 'styling-api' },
-  { fileName: 'utility-functions.md', id: 'utility-functions' },
+  { fileName: 'recipes-api.md', id: 'recipes-api' },
   { fileName: 'dynamic-api.md', id: 'dynamic-api' },
+  { fileName: 'utility-functions.md', id: 'utility-functions' },
   { fileName: 'setup.md', id: 'setup' },
 ];
