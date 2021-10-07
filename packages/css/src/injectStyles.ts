d2fd45cc71ec2ec0bf22f4767574b0ebcd2aca51
Ben Jervis <bjervis@seek.com.au>
2021-10-07 16:11:40 +1100

    Add the inject styles entry point

---
+++ packages/css/src/injectStyles.ts
@@ -0,0 +1,29 @@
+import type { FileScope } from './types';
+
+const stylesheets: Record<string, HTMLElement> = {};
+
+interface InjectStylesOptions {
+  fileScope: FileScope;
+  css: string;
+}
+export const injectStyles = ({ fileScope, css }: InjectStylesOptions) => {
+  const fileScopeId = fileScope.packageName
+    ? [fileScope.packageName, fileScope.filePath].join('/')
+    : fileScope.filePath;
+
+  let stylesheet = stylesheets[fileScopeId];
+
+  if (!stylesheet) {
+    const styleEl = document.createElement('style');
+
+    if (fileScope.packageName) {
+      styleEl.setAttribute('data-package', fileScope.packageName);
+    }
+
+    styleEl.setAttribute('data-file', fileScope.filePath);
+    stylesheet = stylesheets[fileScopeId] = styleEl;
+    document.head.appendChild(styleEl);
+  }
+
+  stylesheet.innerHTML = css;
+};
