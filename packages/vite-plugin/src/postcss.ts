85e9bbcd29f3a7b7eda893631f602ad0e3a5602b
Ben Jervis <bjervis@seek.com.au>
2021-12-01 18:10:42 +1100

    Process vanilla files with postcss during dev mode

---
+++ packages/vite-plugin/src/postcss.ts
@@ -0,0 +1,46 @@
+import type { ResolvedConfig } from 'vite';
+import postcssrc from 'postcss-load-config';
+import type { ProcessOptions, Plugin } from 'postcss';
+
+export interface PostCSSConfigResult {
+  options: ProcessOptions;
+  plugins: Plugin[];
+}
+
+// Mostly copied from vite's implementation
+// https://github.com/vitejs/vite/blob/efec70f816b80e55b64255b32a5f120e1cf4e4be/packages/vite/src/node/plugins/css.ts
+export const resolvePostcssConfig = async (
+  config: ResolvedConfig,
+): Promise<PostCSSConfigResult | null> => {
+  // inline postcss config via vite config
+  const inlineOptions = config.css?.postcss;
+  const inlineOptionsIsString = typeof inlineOptions === 'string';
+
+  if (inlineOptions && !inlineOptionsIsString) {
+    const options = { ...inlineOptions };
+
+    delete options.plugins;
+    return {
+      options,
+      plugins: inlineOptions.plugins || [],
+    };
+  } else {
+    try {
+      const searchPath =
+        typeof inlineOptions === 'string' ? inlineOptions : config.root;
+
+      const postCssConfig = await postcssrc({}, searchPath);
+
+      return {
+        options: postCssConfig.options,
+        // @ts-expect-error - The postcssrc options don't agree with real postcss, but it should be ok
+        plugins: postCssConfig.plugins,
+      };
+    } catch (e: any) {
+      if (!/No PostCSS Config found/.test(e.message)) {
+        throw e;
+      }
+      return null;
+    }
+  }
+};
