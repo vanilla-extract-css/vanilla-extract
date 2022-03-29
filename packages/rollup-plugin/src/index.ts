3506d09131db59328afadc9188cfda03d9e2264b
Paul Grau <paul@graycoding.com>
2022-03-29 23:21:52 +0900

    Simplify

--- packages/rollup-plugin/src/index.ts
+++ packages/rollup-plugin/src/index.ts
@@ -5,12 +5,11 @@ import {
   compile,
   IdentifierOption,
   getSourceFromVirtualCssFile,
-  serializeCss,
+  virtualCssFileFilter,
 } from '@vanilla-extract/integration';
 import { relative, normalize, dirname } from 'path';
 
 const virtualPrefix = 'virtual:vanilla-extract:';
-const vanillaSuffix = '.vanilla.css';
 
 interface Options {
   identifiers?: IdentifierOption;
@@ -22,12 +21,6 @@ export function vanillaExtractPlugin({ identifiers, cwd = process.cwd() }: Optio
 
   return {
     name: 'vanilla-extract',
-    outputOptions(outputOptions) {
-      if (outputOptions.format && !['es', 'esm', 'module'].includes(outputOptions.format)) {
-        this.warn('Only format \'esm\' is supported, others should NOT be used with this plugin');
-      }
-      return outputOptions;
-    },
     async transform(_code, id) {
       if (!cssFileFilter.test(id)) {
         return null;
@@ -49,21 +42,15 @@ export function vanillaExtractPlugin({ identifiers, cwd = process.cwd() }: Optio
         source,
         filePath,
         identOption: identifiers ?? (isProduction ? 'short' : 'debug'),
-        async serializeVirtualCssPath({ fileScope, source }) {
-          // This is almost identical to processVanillaFile's default behavior,
-          // minus the package name in the fileName, in order to
-          // place emitted assets at more sensible paths.
-          const serializedCss = await serializeCss(source);
-          const fileName = `${fileScope.filePath}${vanillaSuffix}`;
-          return `import '${fileName}?source=${serializedCss}';`;
-        }
       });
     },
     async resolveId(id) {
-      // Resolve '.vanilla.css' imports
-      if (id.indexOf(`${vanillaSuffix}?source`) === -1) return null;
+      // Only handle virtual css modules here
+      if (!virtualCssFileFilter.test(id)) {
+        return null;
+      }
 
-      // Emit the CSS file
+      // Emit the css asset
       const { fileName, source } = await getSourceFromVirtualCssFile(id);
       if (!emittedFiles.get(fileName)) {
         const assetId = this.emitFile({
@@ -82,7 +69,7 @@ export function vanillaExtractPlugin({ identifiers, cwd = process.cwd() }: Optio
       };
     },
     renderChunk(code, chunkInfo) {
-      // Replace external virtual imports with emitted CSS files
+      // Replace external virtual imports with emitted css files
       const chunkPath = dirname(chunkInfo.fileName);
       const importsToReplace = chunkInfo.imports.filter((i) => i.startsWith(virtualPrefix));
       if (!importsToReplace.length) return null;
