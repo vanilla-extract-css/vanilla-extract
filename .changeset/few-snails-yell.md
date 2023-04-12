24c37e473b6792d0439381f89bf2864805b0e613
SuttonJack <jack@momenthouse.com>
2023-04-11 23:21:36 -0400

    [next-plugin]: add hasAppDir to next css loader config
    Co-authored-by: Paul Grau <graup@users.noreply.github.com>

---
+++ .changeset/few-snails-yell.md
@@ -0,0 +1,5 @@
+---
+'@vanilla-extract/next-plugin': patch
+---
+
+Prepare next-plugin for Next 13 App Dir support
--- packages/next-plugin/src/index.ts
+++ packages/next-plugin/src/index.ts
@@ -47,6 +47,7 @@ export const createVanillaExtractPlugin =
               isDevelopment: dev,
               future: nextConfig.future || {},
               experimental: nextConfig.experimental || {},
+              hasAppDir: nextConfig.experimental?.appDir,
             } as any,
             () => lazyPostCSS(dir, getSupportedBrowsers(dir, dev), undefined),
             [],
