a8f0845a03f31c95b6675e28c29f81fb1d40efb8
Adam Skoufis <adam.skoufis@gmail.com>
2023-10-31 20:19:40 +1100

    Revert `MapLeafNodes` change

--- .changeset/fast-wombats-joke.md
+++ .changeset/fast-wombats-joke.md
@@ -1,6 +1,5 @@
 ---
 '@vanilla-extract/dynamic': patch
-'@vanilla-extract/private': patch
 ---
 
 assignInlineVars can accept undefined values
--- packages/private/src/types.ts
+++ packages/private/src/types.ts
@@ -11,7 +11,7 @@ type Primitive = string | boolean | number | null | undefined;
 export type MapLeafNodes<Obj, LeafType> = {
   [Prop in keyof Obj]: Obj[Prop] extends Primitive
     ? LeafType
-    : Obj[Prop] extends Record<string | number, any> | undefined
+    : Obj[Prop] extends Record<string | number, any>
     ? MapLeafNodes<Obj[Prop], LeafType>
     : never;
 };
