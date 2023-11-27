4cd53bc92bb1942978d83519f7ef042d3da0fc86
Remus Mate <rmate@seek.com.au>
2023-11-28 10:54:13 +1100

    make recipes work

--- playground/src/shims/require-like.cjs
+++ playground/src/shims/require-like.cjs
@@ -3,6 +3,7 @@ const cache = {
   '@vanilla-extract/css/adapter': require('@vanilla-extract/css/adapter'),
   '@vanilla-extract/css/fileScope': require('@vanilla-extract/css/fileScope'),
   '@vanilla-extract/css-utils': require('@vanilla-extract/css-utils'),
+  '@vanilla-extract/recipes': require('@vanilla-extract/recipes'),
 };
 
 module.exports = () => (id) => cache[id];
