bf42b6482436df6f76864c8de5d9e437bfb02723
Remus Mate <rmate@seek.com.au>
2023-12-17 19:17:55 +1100

    add cli

---
+++ playground/src/cli.ts
@@ -0,0 +1,21 @@
+import fs from 'node:fs/promises';
+import parseArgs from 'minimist';
+
+import { initialize, compile } from './index';
+
+const {
+  _: [fileName],
+  identifiers,
+} = parseArgs(process.argv.slice(2));
+
+(async () => {
+  await initialize({});
+
+  const { css } = await compile({
+    filePath: 'styles.css.ts',
+    input: await fs.readFile(fileName, 'utf-8'),
+    identifiers,
+  });
+
+  console.log(css);
+})();
