6d29ca1e76d86c96d9e5686c140afc021d19d3a7
Remus Mate <rmate@seek.com.au>
2023-12-20 19:11:16 +1100

    refactor `addFileScope` to be able to add the global adapter on already transformed files

--- packages/integration/src/addFileScope.ts
+++ packages/integration/src/addFileScope.ts
@@ -1,5 +1,10 @@
-import { posix, relative, sep } from 'path';
+import { posix, win32 } from 'path';
 import { detectSyntax } from 'mlly';
+
+// Inlined from @rollup/pluginutils
+// https://github.com/rollup/plugins/blob/33174f956304ab4aad4bbaba656f627c31679dc5/packages/pluginutils/src/normalizePath.ts#L5-L7
+export const normalizePath = (filename: string) =>
+  filename.split(win32.sep).join(posix.sep);
 
 interface AddFileScopeParams {
   source: string;
@@ -11,59 +16,60 @@ interface AddFileScopeParams {
 export function addFileScope({
   source,
   filePath,
-  rootPath,
   packageName,
   globalAdapterIdentifier,
 }: AddFileScopeParams) {
   // Encode windows file paths as posix
-  const normalizedPath = posix.join(...relative(rootPath, filePath).split(sep));
+  const normalizedPath = normalizePath(filePath);
 
-  if (source.indexOf('@vanilla-extract/css/fileScope') > -1) {
-    return source.replace(
+  // If there's already a file scope set, replace it with the current file scope
+  if (source.includes('@vanilla-extract/css/fileScope')) {
+    source = source.replace(
       /setFileScope\(((\n|.)*?)\)/,
       `setFileScope("${normalizedPath}", "${packageName}")`,
     );
   }
 
+  // If there's an adapter already set, don't add another one
+  if (source.includes('@vanilla-extract/css/adapter')) {
+    return source;
+  }
+
   const { hasESM, isMixed } = detectSyntax(source);
 
-  if (hasESM && !isMixed) {
-    const setAdapterSnippet = globalAdapterIdentifier
-      ? `
-        import * as __vanilla_css_adapter__ from "@vanilla-extract/css/adapter";
-        __vanilla_css_adapter__.setAdapter(${globalAdapterIdentifier});
-      `
-      : '';
-    const removeAdapterSnippet = setAdapterSnippet
-      ? '__vanilla_css_adapter__.removeAdapter();'
-      : '';
+  // If the file has already been transformed, don't add another file scope
+  if (!source.includes('@vanilla-extract/css/fileScope')) {
+    if (hasESM && !isMixed) {
+      source = `
+        import { setFileScope, endFileScope } from "@vanilla-extract/css/fileScope";
+        setFileScope("${normalizedPath}", "${packageName}");
+        ${source}
+        endFileScope();
+      `;
+    } else {
+      source = `
+        const __vanilla_filescope__ = require("@vanilla-extract/css/fileScope");
+        __vanilla_filescope__.setFileScope("${normalizedPath}", "${packageName}");
+        ${source}
+        __vanilla_filescope__.endFileScope();
+      `;
+    }
+  }
 
-    return `
-      ${setAdapterSnippet}
-      import { setFileScope, endFileScope } from "@vanilla-extract/css/fileScope";
-      setFileScope("${normalizedPath}", "${packageName}");
+  // Add the global adapter
+  if (globalAdapterIdentifier) {
+    const adapterImport =
+      hasESM && !isMixed
+        ? 'import * as __vanilla_css_adapter__ from "@vanilla-extract/css/adapter";'
+        : 'const __vanilla_css_adapter__ = require("@vanilla-extract/css/adapter");';
+
+    source = `
+      ${adapterImport}
+      __vanilla_css_adapter__.setAdapter(${globalAdapterIdentifier});
       ${source}
-      endFileScope();
-      ${removeAdapterSnippet}
+      __vanilla_css_adapter__.removeAdapter();
     `;
   }
 
-  const setAdapterSnippet = globalAdapterIdentifier
-    ? `
-      const __vanilla_css_adapter__ = require("@vanilla-extract/css/adapter");
-      __vanilla_css_adapter__.setAdapter(${globalAdapterIdentifier});
-    `
-    : '';
-  const removeAdapterSnippet = setAdapterSnippet
-    ? '__vanilla_css_adapter__.removeAdapter();'
-    : '';
-
-  return `
-    ${setAdapterSnippet}
-    const __vanilla_filescope__ = require("@vanilla-extract/css/fileScope");
-    __vanilla_filescope__.setFileScope("${normalizedPath}", "${packageName}");
-    ${source}
-    __vanilla_filescope__.endFileScope();
-    ${removeAdapterSnippet};
-  `;
+  return source;
 }
