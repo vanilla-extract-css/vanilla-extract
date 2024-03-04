1cd79e4433cb082f2c17a98d9d864e92867bad8d
Adam Skoufis <adam.skoufis@gmail.com>
2024-03-04 20:19:13 +1100

    Update remix example dependencies

--- examples/remix/vite.config.ts
+++ examples/remix/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import { unstable_vitePlugin as remix } from '@remix-run/dev';
+import { vitePlugin as remix } from '@remix-run/dev';
 
 export default defineConfig({
   css: {
