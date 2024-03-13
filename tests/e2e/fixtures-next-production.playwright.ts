ccb4957b549107394cd6867da71b3e979c96a28d
Ren <renrizmusic@gmail.com>
2024-03-14 09:49:06 +1100

    fix(e2e): next snapshot tests (#1349)
    Co-authored-by: Adam Skoufis <askoufis@seek.com.au>

---
+++ tests/e2e/fixtures-next-production.playwright.ts
@@ -0,0 +1,46 @@
+import {
+  nextFixtures,
+  startFixture,
+  TestServer,
+} from '@vanilla-extract-private/test-helpers';
+import { expect } from '@playwright/test';
+
+import test from './fixture';
+
+const testCases = [
+  {
+    type: 'next-pages-router',
+    mode: 'production',
+  },
+  {
+    type: 'next-app-router',
+    mode: 'production',
+  },
+] as const;
+
+testCases.forEach(({ type, mode }) => {
+  test.describe.serial(`${type} (${mode})`, async () => {
+    let server: TestServer;
+
+    test.beforeAll(async ({ port }) => {
+      server = await startFixture('', {
+        type,
+        basePort: port,
+        mode,
+      });
+    });
+
+    test(`screenshot`, async ({ page }) => {
+      for await (const fixture of nextFixtures) {
+        await page.goto(`${server.url}/${fixture}`);
+        // make sure the fixture is visible
+        await page.waitForSelector(`#${fixture}`, { state: 'attached' });
+        expect(await page.screenshot()).toMatchSnapshot(`${fixture}.png`);
+      }
+    });
+
+    test.afterAll(async () => {
+      await server.close();
+    });
+  });
+});
