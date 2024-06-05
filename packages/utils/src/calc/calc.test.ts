6ea9740a45e692831512707c6ab43a78d97a81ba
Rowan Gudmundsson <rowan.gudmunds@gmail.com>
2024-06-04 20:35:57 -0700

    refactor: change calc to use a class based imp

---
+++ packages/utils/src/calc/calc.test.ts
@@ -0,0 +1,159 @@
+import { calc } from './calc';
+
+const css = 'var(--some-css-var)';
+
+describe('class UnitCalc', () => {
+  describe('add()', () => {
+    it.each`
+      args                     | expectedExpressions | expectedBuilt
+      ${[]}                    | ${['1']}            | ${'calc(1)'}
+      ${[2]}                   | ${['1', '2']}       | ${'calc(1 + 2)'}
+      ${[2, '3']}              | ${['1', '2', '3']}  | ${'calc(1 + 2 + 3)'}
+      ${[calc(2)]}             | ${['1', '2']}       | ${'calc(1 + 2)'}
+      ${[calc(2).add(3)]}      | ${['1', '2 + 3']}   | ${'calc(1 + 2 + 3)'}
+      ${[calc(2).subtract(3)]} | ${['1', '2 - 3']}   | ${'calc(1 + 2 - 3)'}
+      ${[calc(2).multiply(3)]} | ${['1', '2 * 3']}   | ${'calc(1 + 2 * 3)'}
+      ${[calc(2).divide(3)]}   | ${['1', '2 / 3']}   | ${'calc(1 + 2 / 3)'}
+      ${[css]}                 | ${['1', css]}       | ${'calc(1 + var(--some-css-var))'}
+    `(
+      'should add $args to 1 to get $expectedBuilt',
+      ({ args, expectedExpressions, expectedBuilt }) => {
+        const result = calc(1).add(...args);
+
+        expect(result).toMatchObject({
+          expressions: expectedExpressions,
+          operator: '+',
+        });
+        expect(result.build()).toBe(expectedBuilt);
+      },
+    );
+  });
+
+  describe('subtract()', () => {
+    it.each`
+      args                     | expectedExpressions | expectedBuilt
+      ${[]}                    | ${['1']}            | ${'calc(1)'}
+      ${[2]}                   | ${['1', '2']}       | ${'calc(1 - 2)'}
+      ${[2, '3']}              | ${['1', '2', '3']}  | ${'calc(1 - 2 - 3)'}
+      ${[calc(2)]}             | ${['1', '2']}       | ${'calc(1 - 2)'}
+      ${[calc(2).add(3)]}      | ${['1', '2 + 3']}   | ${'calc(1 - 2 + 3)'}
+      ${[calc(2).subtract(3)]} | ${['1', '2 - 3']}   | ${'calc(1 - 2 - 3)'}
+      ${[calc(2).multiply(3)]} | ${['1', '2 * 3']}   | ${'calc(1 - 2 * 3)'}
+      ${[calc(2).divide(3)]}   | ${['1', '2 / 3']}   | ${'calc(1 - 2 / 3)'}
+      ${[css]}                 | ${['1', css]}       | ${'calc(1 - var(--some-css-var))'}
+    `(
+      'should subtract $args from 1 to get $expectedBuilt',
+      ({ args, expectedExpressions, expectedBuilt }) => {
+        const result = calc(1).subtract(...args);
+
+        expect(result).toMatchObject({
+          expressions: expectedExpressions,
+          operator: '-',
+        });
+        expect(result.build()).toBe(expectedBuilt);
+      },
+    );
+  });
+
+  describe('multiply()', () => {
+    it.each`
+      args                     | expectedExpressions | expectedBuilt
+      ${[]}                    | ${['1']}            | ${'calc(1)'}
+      ${[2]}                   | ${['1', '2']}       | ${'calc(1 * 2)'}
+      ${[2, '3']}              | ${['1', '2', '3']}  | ${'calc(1 * 2 * 3)'}
+      ${[calc(2)]}             | ${['1', '2']}       | ${'calc(1 * 2)'}
+      ${[calc(2).add(3)]}      | ${['1', '(2 + 3)']} | ${'calc(1 * (2 + 3))'}
+      ${[calc(2).subtract(3)]} | ${['1', '(2 - 3)']} | ${'calc(1 * (2 - 3))'}
+      ${[calc(2).multiply(3)]} | ${['1', '2 * 3']}   | ${'calc(1 * 2 * 3)'}
+      ${[calc(2).divide(3)]}   | ${['1', '2 / 3']}   | ${'calc(1 * 2 / 3)'}
+      ${[css]}                 | ${['1', css]}       | ${'calc(1 * var(--some-css-var))'}
+    `(
+      'should multiply 1 by $args to get $expectedBuilt',
+      ({ args, expectedExpressions, expectedBuilt }) => {
+        const result = calc(1).multiply(...args);
+
+        expect(result).toMatchObject({
+          expressions: expectedExpressions,
+          operator: '*',
+        });
+        expect(result.build()).toBe(expectedBuilt);
+      },
+    );
+  });
+
+  describe('divide()', () => {
+    it.each`
+      args                     | expectedExpressions | expectedBuilt
+      ${[]}                    | ${['1']}            | ${'calc(1)'}
+      ${[2]}                   | ${['1', '2']}       | ${'calc(1 / 2)'}
+      ${[2, '3']}              | ${['1', '2', '3']}  | ${'calc(1 / 2 / 3)'}
+      ${[calc(2)]}             | ${['1', '2']}       | ${'calc(1 / 2)'}
+      ${[calc(2).add(3)]}      | ${['1', '(2 + 3)']} | ${'calc(1 / (2 + 3))'}
+      ${[calc(2).subtract(3)]} | ${['1', '(2 - 3)']} | ${'calc(1 / (2 - 3))'}
+      ${[calc(2).multiply(3)]} | ${['1', '2 * 3']}   | ${'calc(1 / 2 * 3)'}
+      ${[calc(2).divide(3)]}   | ${['1', '2 / 3']}   | ${'calc(1 / 2 / 3)'}
+      ${[css]}                 | ${['1', css]}       | ${'calc(1 / var(--some-css-var))'}
+    `(
+      'should divide 1 by $args to get $expectedBuilt',
+      ({ args, expectedExpressions, expectedBuilt }) => {
+        const result = calc(1).divide(...args);
+
+        expect(result).toMatchObject({
+          expressions: expectedExpressions,
+          operator: '/',
+        });
+        expect(result.build()).toBe(expectedBuilt);
+      },
+    );
+  });
+
+  describe('negate()', () => {
+    it.each`
+      calc                   | expectedExpressions  | expectedBuilt
+      ${calc(1)}             | ${['1', '-1']}       | ${'calc(1 * -1)'}
+      ${calc(1).add(3)}      | ${['(1 + 3)', '-1']} | ${'calc((1 + 3) * -1)'}
+      ${calc(1).subtract(3)} | ${['(1 - 3)', '-1']} | ${'calc((1 - 3) * -1)'}
+      ${calc(1).multiply(3)} | ${['1 * 3', '-1']}   | ${'calc(1 * 3 * -1)'}
+      ${calc(1).divide(3)}   | ${['1 / 3', '-1']}   | ${'calc(1 / 3 * -1)'}
+      ${calc(css)}           | ${[css, '-1']}       | ${'calc(var(--some-css-var) * -1)'}
+    `(
+      'should negate $calc to get $expectedBuilt',
+      ({ calc, expectedExpressions, expectedBuilt }) => {
+        const result = calc.negate();
+
+        expect(result).toMatchObject({
+          expressions: expectedExpressions,
+          operator: '*',
+        });
+        expect(result.build()).toBe(expectedBuilt);
+      },
+    );
+  });
+
+  describe('build()', () => {
+    it.each`
+      calc                   | expected
+      ${calc(1)}             | ${'calc(1)'}
+      ${calc(1).add(3)}      | ${'calc(1 + 3)'}
+      ${calc(1).subtract(3)} | ${'calc(1 - 3)'}
+      ${calc(1).multiply(3)} | ${'calc(1 * 3)'}
+      ${calc(1).divide(3)}   | ${'calc(1 / 3)'}
+      ${calc(1).negate()}    | ${'calc(1 * -1)'}
+    `('should build $calc to get $expected', ({ calc, expected }) => {
+      expect(calc.build()).toBe(expected);
+    });
+  });
+
+  describe('toString()', () => {
+    it.each`
+      calc                   | expected
+      ${calc(1)}             | ${'1'}
+      ${calc(1).add(3)}      | ${'1 + 3'}
+      ${calc(1).subtract(3)} | ${'1 - 3'}
+      ${calc(1).multiply(3)} | ${'1 * 3'}
+      ${calc(1).divide(3)}   | ${'1 / 3'}
+    `('should convert $calc to string $expected', ({ calc, expected }) => {
+      expect(calc.toString()).toBe(expected);
+    });
+  });
+});
---
+++ packages/utils/src/calc/calc.ts
@@ -0,0 +1,211 @@
+/**
+ * @file Defines a class for creating CSS calc() functions. This class allows
+ * for chaining operations to build up a calc() function.
+ * @author rowan-gud
+ * @author michaeltaranto
+ * @author markdalgleish
+
+/**
+ * An input to a calc function. Can be a number, string, or another Calc
+ * instance.
+ *
+ * NOTE: Unlike a lot of other places in this library, numbers will not be
+ * interpreted as pixel values since they can be used to represent unitless
+ * values.
+ */
+type Operand = string | number | Calc;
+
+class Calc {
+  expressions: string[];
+
+  private static isAddSubCalc(expr: Operand): expr is Calc {
+    return expr instanceof Calc && Calc.isAddSub(expr);
+  }
+
+  private static isAddSub(calc: Calc): boolean {
+    return calc.operator === '+' || calc.operator === '-';
+  }
+
+  constructor(
+    exprs: Operand[],
+    private readonly operator?: '+' | '-' | '*' | '/' | '',
+  ) {
+    this.expressions = exprs.map((e) => e.toString());
+  }
+
+  /**
+   * Add operands to the current Calc instance
+   *
+   * @param operands - The operands to add
+   * @returns A new Calc instance with the added operands
+   * @example
+   * ```ts
+   * const result = calc(1).add(3);
+   *
+   * expect(result.toString()).toBe('1 + 3');
+   * expect(result.build()).toBe('calc(1 + 3)');
+   * ```
+   */
+  public add(...operands: Operand[]): Calc {
+    return new Calc([this.toString(), ...operands], '+');
+  }
+
+  /**
+   * Subtract operands from the current Calc instance
+   *
+   * @param operands - The operands to subtract
+   * @returns A new Calc instance with the subtracted operands
+   * @example
+   * ```ts
+   * const result = calc(1).subtract(3);
+   *
+   * expect(result.toString()).toBe('1 - 3');
+   * expect(result.build()).toBe('calc(1 - 3)');
+   * ```
+   */
+  public subtract(...operands: Operand[]): Calc {
+    return new Calc([this.toString(), ...operands], '-');
+  }
+
+  /**
+   * Multiply operands with the current Calc instance. If any of the operands
+   * are Calc instances with the operator '+' or '-', they will be wrapped in
+   * parentheses.
+   *
+   * @param operands - The operands to multiply
+   * @returns A new Calc instance with the multiplied operands
+   * @example
+   * ```ts
+   * const result = calc(1).multiply(3);
+   *
+   * expect(result.toString()).toBe('1 * 3');
+   * expect(result.build()).toBe('calc(1 * 3)');
+   *
+   * const result2 = calc(1).add(2, 3).multiply(4);
+   *
+   * expect(result2.toString()).toBe('(1 + 2 + 3) * 4');
+   * expect(result2.build()).toBe('calc((1 + 2 + 3) * 4)');
+   * ```
+   */
+  public multiply(...operands: Operand[]): Calc {
+    return new Calc(
+      [
+        this.toString(),
+        ...operands.map((e) =>
+          Calc.isAddSubCalc(e) ? `(${e.toString()})` : e,
+        ),
+      ],
+      '*',
+    );
+  }
+
+  /**
+   * Divide the current Calc instance by the operands. If any of the operands
+   * are Calc instances with the operator '+' or '-', they will be wrapped in
+   * parentheses.
+   *
+   * @param operands - The operands to divide by
+   * @returns A new Calc instance with the divided operands
+   * @example
+   * ```ts
+   * const result = calc(1).divide(2);
+   *
+   * expect(result.toString()).toBe('1 / 2');
+   * expect(result.build()).toBe('calc(1 / 2)');
+   * ```
+   */
+  public divide(...operands: Operand[]): Calc {
+    return new Calc(
+      [
+        this.toString(),
+        ...operands.map((e) =>
+          Calc.isAddSubCalc(e) ? `(${e.toString()})` : e,
+        ),
+      ],
+      '/',
+    );
+  }
+
+  /**
+   * Negate the current Calc instance. If the current Calc instance has the
+   * operator '+' or '-', it will be wrapped in parentheses.
+   *
+   * @returns A new Calc instance with the negated value
+   * @example
+   * ```ts
+   * const result = calc(1).negate();
+   *
+   * expect(result.toString()).toBe('1 * -1');
+   * expect(result.build()).toBe('calc(1 * -1)');
+   *
+   * const result2 = calc(1).add(3).negate();
+   *
+   * expect(result2.toString()).toBe('(1 + 3) * -1');
+   * expect(result2.build()).toBe('calc((1 + 3) * -1)');
+   * ```
+   */
+  public negate(): Calc {
+    return new Calc(
+      [Calc.isAddSub(this) ? `(${this.toString()})` : this.toString(), -1],
+      '*',
+    );
+  }
+
+  /**
+   * Build the current Calc instance into a CSS calc() function
+   *
+   * @returns The CSS calc() function
+   * @example
+   * ```ts
+   * const result = calc(1).add(3);
+   *
+   * expect(result.build()).toBe('calc(1 + 3)');
+   * ```
+   */
+  public build(): string {
+    return `calc(${this.toString()})`;
+  }
+
+  /**
+   * Convert the current Calc instance to a string
+   *
+   * NOTE: This is not the same as calling build(). The resulting string will
+   * not be wrapped in a calc() function
+   *
+   * @returns The string representation of the Calc instance
+   * @example
+   * ```ts
+   * const result = calc(1).add(3);
+   *
+   * expect(result.toString()).toBe('1 + 3');
+   * ```
+   */
+  public toString(): string {
+    if (this.operator === undefined) {
+      return this.expressions[0] ?? '';
+    }
+
+    return this.expressions.join(` ${this.operator} `);
+  }
+}
+
+/**
+ * Create a new Calc instance with the given operand
+ *
+ * @param x - The operand to create the Calc instance with
+ * @returns A new Calc instance
+ * @example
+ * ```ts
+ * const headerHeight = createVar()
+ *
+ * const root = style({
+ *  height: calc(headerHeight)
+ *    .divide(2)
+ *    .build(),
+ * })
+ * ```
+ */
+export function calc(x: Operand): Calc {
+  // "Ensure" that this is the only way to create a Calc instance
+  return new Calc([x]);
+}
