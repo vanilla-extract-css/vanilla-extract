9813a63a8f41f2924bb464b504af3ae1560f5d84
Ray Knight <array.knight+github@gmail.com>
2024-11-07 12:24:23 -0800

    feat: 🎸 expose esbuild log options

--- packages/integration/src/compile.ts
+++ packages/integration/src/compile.ts
@@ -49,7 +49,7 @@ export interface CompileOptions {
   cwd?: string;
   esbuildOptions?: Pick<
     EsbuildOptions,
-    'plugins' | 'external' | 'define' | 'loader' | 'tsconfig' | 'conditions'
+    'plugins' | 'external' | 'define' | 'loader' | 'tsconfig' | 'conditions' | 'logLevel' | 'logLimit' | 'logOverride'
   >;
 }
 export async function compile({
@@ -74,6 +74,9 @@ export async function compile({
     define: esbuildOptions?.define,
     tsconfig: esbuildOptions?.tsconfig,
     conditions: esbuildOptions?.conditions,
+    logLevel: esbuildOptions?.logLevel,
+    logLimit: esbuildOptions?.logLimit,
+    logOverride: esbuildOptions?.logOverride,
   });
 
   const { outputFiles, metafile } = result;
