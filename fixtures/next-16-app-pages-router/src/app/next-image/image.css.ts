f796980a51abbd29d4b457c2da0c7a39482dcc64
Robbie <robbiethewadley@gmail.com>
2025-12-10 11:46:08 -0700

    typecast

--- fixtures/next-16-app-pages-router/src/app/next-image/image.css.ts
+++ fixtures/next-16-app-pages-router/src/app/next-image/image.css.ts
@@ -1,9 +1,13 @@
 import logo from './logo.png';
 import { style } from '@vanilla-extract/css';
+import type { StaticImageData } from 'next/image';
+
+// types are funky because of our monorepo setup
+const withCast = logo as unknown as StaticImageData;
 
 export const imageStyle = style({
-  backgroundImage: `url(${logo.src})`,
-  width: `${logo.width}px`,
-  height: `${logo.height}px`,
+  backgroundImage: `url(${withCast.src})`,
+  width: `${withCast.width}px`,
+  height: `${withCast.height}px`,
   border: '1px solid red',
 });
